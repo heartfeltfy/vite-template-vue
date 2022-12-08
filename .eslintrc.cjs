69fc617b8b783e71e364eb88e3d346e35d32a5f7
CMF-chenmengfei <15239905718@163.com>
2022-12-08 14:38:30 +0800

    更新eslint规则，代码结尾不需要空行

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,9 +26,11 @@ module.exports = {
     'indent' : ['error' , 2] , // 强制使用一致的缩进
     'default-case' : 2 , // 强制switch要有default分支
     'camelcase' : 2 , // 强制使用骆驼拼写法命名约定
+    'eol-last' : 0 ,//代码结尾不需要空行
     'space-before-function-paren' : [0 , 'always'] , //函数定义时括号前面要不要有空格
     // 优先使用 interface 而不是 type
     '@typescript-eslint/consistent-type-definitions' : ['error' , 'interface'] ,
+    'no-multiple-empty-lines' : ['error' , { 'max' : 2 , 'maxEOF' : 1 }] ,
     '@typescript-eslint/no-explicit-any' : 'off' ,
     '@typescript-eslint/triple-slash-reference' : 'off' ,
     '@typescript-eslint/explicit-module-boundary-types' : 'off' ,
@@ -36,8 +38,8 @@ module.exports = {
     '@typescript-eslint/strict-boolean-expressions' : 'off' ,
     '@typescript-eslint/prefer-optional-chain' : 'off' ,
     '@typescript-eslint/no-unused-vars' : 'off' ,
-    '@typescript-eslint/quotes' : 'off' ,
-    '@typescript-eslint/semi' : 'off' ,
+    // '@typescript-eslint/quotes' : 'off' ,
+    // '@typescript-eslint/semi' : 'off' ,
     '@typescript-eslint/no-extraneous-class' : 'off'
   }
 }
